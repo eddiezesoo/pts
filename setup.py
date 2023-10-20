62b9cb893f52a6766649d30df766d642b583e99f
zesoo <zhuangyisheng@qq.com>
2023-10-20 20:29:42 +0800

    zesoo_2310202029

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 
 setup(name='pts',
-      version='1.0.10',
+      version='1.0.11',
       description='py tools by zesoo',
       author='zesoo',
       author_email='zhuangyisheng@qq.com',
