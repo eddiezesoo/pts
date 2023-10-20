26febc32845b5275c61052021be38b3af13fa9de
zesoo <zhuangyisheng@qq.com>
2023-10-20 19:55:22 +0800

    zesoo_2310201955

---
+++ pts/date_time/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+# Author zhuangyisheng@qq.com 2023/10/20
+from . import full_year
---
+++ pts/date_time/full_year.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Author zhuangyisheng@qq.com 2023/10/20
+
+
+def full_year(start_date, end_date):
+    if start_date.month * 100 + start_date.day < end_date.month * 100 + end_date.day:
+        return end_date.year - start_date.year
+    else:
+        return end_date.year - start_date.year - 1
