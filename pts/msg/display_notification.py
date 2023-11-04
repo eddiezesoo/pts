610e448e391b1a7b715d94c6daccb9c59b151a72
zesoo <zhuangyisheng@qq.com>
2023-11-04 09:25:54 +0800

    zesoo_2311040925

--- pts/msg/display_notification.py
+++ pts/msg/display_notification.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # Author zhuangyisheng@qq.com 2023/10/10
 
-def notify(message, title, msg_type):
-    return {
+def notify(message, title, msg_type, close):
+    msg = {
         'type': 'ir.actions.client',
         'tag': 'display_notification',
         'params': {
@@ -10,20 +10,23 @@ def notify(message, title, msg_type):
             'message': message,
             'sticky': False,  # True 手动关闭   False 延时关闭
             'type': msg_type,
-            'next': {'type': 'ir.actions.act_window_close'},
         }
     }
+    if close:
+        # 后续关闭动作，可以是其他动作
+        msg.get('params').update({'next': {'type': 'ir.actions.act_window_close'}})
+    return msg
 
-def success(message=None, title='提示'):
-    return notify(message, title, 'success')
+def success(message=None, title='提示', close=False):
+    return notify(message, title, 'success', close)
 
 
-def danger(message=None, title='提示'):
-    return notify(message, title, 'danger')
+def danger(message=None, title='提示', close=False):
+    return notify(message, title, 'danger', close)
 
 
-def warning(message=None, title='提示'):
-    return notify(message, title, 'warning')
+def warning(message=None, title='提示', close=False):
+    return notify(message, title, 'warning', close)
 
 
 
